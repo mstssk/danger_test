733d631bbaae212216c29dec56474f409f8e5207
mstssk <mstssk+github@gmail.com>
2018-03-16 22:38:41 +0900

    レビュアーの有無をチェックしてみる

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,4 +1,9 @@
-import {message, danger} from "danger"
+import {warn, danger} from "danger"
 
-const modifiedMD = danger.git.modified_files.join("- ")
-message("Changed Files in this PR: \n - " + modifiedMD)
+// const modifiedMD = danger.git.modified_files.join("- ")
+// message("Changed Files in this PR: \n - " + modifiedMD)
+
+console.log(danger.github.requested_reviewers);
+if (danger.github.requested_reviewers.length === 0) {
+    warn(`レビュアーを設定してください。`);
+}
