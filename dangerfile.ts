e5bcc5cc8463fe490bf02a69df743b28e356e99d
mstssk <mstssk+github@gmail.com>
2018-03-16 22:46:27 +0900

    wip

--- dangerfile.ts
+++ dangerfile.ts
@@ -3,7 +3,7 @@ import {warn, danger} from "danger"
 // const modifiedMD = danger.git.modified_files.join("- ")
 // message("Changed Files in this PR: \n - " + modifiedMD)
 
-console.log(danger.github.requested_reviewers);
+console.log(danger.github);
 if (danger.github.requested_reviewers.length === 0) {
     warn(`レビュアーを設定してください。`);
 }
